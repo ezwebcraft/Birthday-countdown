67119a2706d35e87681621ce9efc1fee615ccc8f
Elvin Ramirez <real8686@gmail.com>
2018-05-20 20:51:06 -0400

    init skeleton of the project

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Birthday-countdown
 Birthday countdown app from Python Jumpstart by Building 10 Apps
+
+#### Project covers:
+- Function
+- Time spans
+- Dates
+- Time
+- return values (function)
+- Pararmeter  (function)
---
+++ birthday.py
@@ -0,0 +1,19 @@
+
+def print_header():
+    pass
+
+def get_birthday_from_user():
+    pass
+
+def computer_days_between_dates():
+    pass
+
+def print_birthdat_information():
+    pass
+
+def main():
+    print_header()
+    get_birthday_from_user()
+    now = None
+    number_of_days = computer_days_between_dates(bday, now)
+    print_birthdat_information()
